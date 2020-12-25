112c4c04533b7387a0cc47a27175af63d1d76583
hans <i@icool.io>
2020-12-25 19:10:16 +0800

    yahaha

--- message/message.go
+++ message/message.go
@@ -2,15 +2,25 @@ package message
 
 import "time"
 
+type ID [16]byte
+
+// Message
 type Message struct {
-	Meta    *Metadata              // meta data
-	Message map[string]interface{} // message body
+	Meta    *Metadata   // meta data
+	Message interface{} // message body
 }
 
+//Metadata The message metadata
 type Metadata struct {
-	ID        [16]byte    // message ID
+	ID        ID        // message ID
 	Host      string    // message product server
 	ServiceID string    // message product service ID
 	Time      time.Time // message time
 	TrackId   string
+}
+
+func (m *Message) Parse(buf []byte) (err error) {
+	m.Meta = &Metadata{}
+	copy(m.Meta.ID[:], buf[:15])
+	return
 }
